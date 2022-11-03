781ffd6d27f6f907582f767ca946abbd06195a9d
Luuuh <luciana@kazap.com.br>
2022-11-03 16:16:39 -0300

    Mobile - Aula 03

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,7 +2,8 @@ import { NativeBaseProvider, StatusBar } from "native-base";
 import {THEME} from './src/styles/theme'
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from './src/components/Loading';
-import { SignIn } from './src/screens/SignIn';
+import { AuthContextProvider } from "./src/contexts/AuthContext";
+import { Pools } from "./src/screens/Pools";
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
@@ -10,12 +11,14 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Pools /> : <Loading />}
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
